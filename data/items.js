c81b21050cbd083580abfa866ed8dfe6d65e8ea5
bousenkeed <148900656+bousenkeed@users.noreply.github.com>
2025-01-23 06:34:11 +0300

    Add files via upload

---
+++ data/items.js
@@ -0,0 +1,46 @@
+export const items = [
+    {
+        title: 'Встраиваемый светильник Markt',
+        src: 'images/1.jpg',
+        price: 3490,
+        promotion: 5060,
+    },
+    {
+        title: 'Линейный светильник ARG',
+        src: 'images/2.jpg',
+        price: 6700,
+    },
+    {
+        title: 'Сведодиодный светильник ',
+        src: 'images/3.jpg',
+        price: 3490,
+        promotion: 6060,
+    },
+    {
+        title: 'Встраиваемый светильник Markt',
+        src: 'images/4.jpg',
+        price: 3490,
+    },
+    {
+        title: 'Линейный светильник ARG',
+        src: 'images/5.jpg',
+        price: 6700,
+        promotion: 6060,
+    },
+    {
+        title: 'Сведодиодный светильник ',
+        src: 'images/6.jpg',
+        price: 5060,
+    },
+    {
+        title: 'Встраиваемый светильник Markt',
+        src: 'images/7.jpg',
+        price: 3490,
+        promotion: 6060,
+    },
+    {
+        title: 'Линейный светильник ARG',
+        src: 'images/8.jpg',
+        price: 6700,
+    },
+];
